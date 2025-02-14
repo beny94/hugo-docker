69b51593765f97bd15a6fcd35ab16d1a6362c7ce
Beny Putra Petir <beny.putra@dev08.harvestindo-global.co.id>
2025-02-14 16:06:41 +0700

    Add gitignore

--- alpine/docker-entrypoint.sh
+++ alpine/docker-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -ex
+
 # Check if /hugo is empty
-if [ ! "$(ls -A /hugo)" ]; then  # ls -A lists all but . and ..; if empty, nothing is returned
+if [ ! "$(ls -A /hugo | grep -v '^\.')" ]; then  # ls -A lists all but . and ..; if empty, nothing is returned
   echo "/hugo is empty. Running hugo new site..."
   hugo new site /hugo
+  cd /hugo
+  git init
+  git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
+  echo "theme = 'ananke'" >> hugo.toml
 else
   echo "/hugo is not empty. Proceeding with normal startup..."
 fi
